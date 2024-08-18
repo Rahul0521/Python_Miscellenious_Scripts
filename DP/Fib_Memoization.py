8492be2635f16a4987d3958981b7a8977049baa1
Rahul Kumar Yadav <38578247+Rahul0521@users.noreply.github.com>
2024-08-18 10:17:59 +0530

    Update Fib_Memoization.py

--- DP/Fib_Memoization.py
+++ DP/Fib_Memoization.py
@@ -1,1 +1,18 @@
+def fib_topDown(n,memo):
+    if memo[n] is not None:
+        return memo[n]
 
+    if n==0 or n==1:
+        result =  n
+    else:
+        result  = fib_topDown(n-1,memo)+fib_topDown(n-2,memo)
+    memo[n] = result
+    return memo[n]
+
+
+def fib_memo(n):
+    memo = [None]*(n+1)
+    return fib_topDown(n,memo)
+
+
+print(fib_memo(7))
